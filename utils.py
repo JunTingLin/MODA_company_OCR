d062b4e99d922d960e54f0ce0bce02b18e957eec
junting <10355098@st.thsh.tp.edu.tw>
2023-12-19 00:14:17 +0800

    Fix file sorting method

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import re
 
 def numerical_sort(filename):
-    """提取檔案名稱中的數字用於排序"""
-    numbers = re.findall(r'\d+', filename)
-    return int(numbers[0]) if numbers else 0
+    """提取文件名中的數字用於排序，先根據文件名前半部分，然後是頁碼"""
+    name_part, page_number = re.match(r'(.*_page_)(\d+)', filename).groups()
+    return (name_part, int(page_number))
