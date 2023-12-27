9826aeed0cef33650c310bf84323f597bfa93ee7
junting <10355098@st.thsh.tp.edu.tw>
2023-12-28 02:18:01 +0800

    Change check_coords and draw a rectangle with specified coordinates on the image

--- checkbox_detector.py
+++ checkbox_detector.py
@@ -1,27 +1,48 @@
 import cv2
 
-def is_checked(image_path):
+def draw_check_coords(image, check_coords):
+    """
+    在圖像上繪製指定坐標的矩形框。
+
+    :param image: 要繪製的圖像
+    :param check_coords: 要繪製的矩形框的坐標列表
+    """
+    # 在每個坐標上繪製矩形框
+    for (x, y, w, h) in check_coords:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)  # 綠色矩形框
+
+    # 顯示圖像
+    cv2.imshow('Check Coords', image)
+    cv2.waitKey(0) # 等待按鍵輸入
+    cv2.destroyAllWindows()
+
+def is_checked(image_path, debug=False):
     """
     檢查指定位置是否有打勾，並處理結果。
 
     :param image_path: 圖像文件的路徑
+    :param debug: 是否顯示除錯視窗
     :return: 處理後的結果字典
     """
     # 固定的 check_coords 坐標
+    # x, y, w, h 分別為左上角 x 座標、左上角 y 座標、寬度、高度
     check_coords = [
-        (1865,595,180,80), (2085,595,180,80),
-        (1865, 705, 180, 45), (2085, 705, 180, 45),
-        (1865, 770, 180, 45), (2085, 770, 180, 45),
-        (1865, 825, 180, 95), (2085, 825, 180, 95),
-        (1865, 945, 180, 95), (2085, 945, 180, 95),
-        (1865, 1053, 180, 95), (2085, 1053, 180, 95),
-        (1865, 1175, 180, 230), (2085, 1175, 180, 230),
-        (1865, 1425, 180, 95), (2085, 1425, 180, 95),
-        (1865, 1624, 180, 375), (2085, 1624, 180, 375),
-        (1865, 2060, 180, 305), (2085, 2060, 180, 305),
-        (1865, 2465, 180, 305), (2085, 2465, 180, 305),
-        (1865, 2805, 180, 130), (2085, 2805, 180, 130),
-        (1865, 3035, 180, 260), (2085, 3035, 180, 260),
+        (1900,611,107,50), (2112,611,107,50),
+        (1900, 670, 107, 50), (2112, 670, 107, 50),
+        (1900, 766, 107, 50), (2112, 766, 107, 50),
+        (1900, 853, 107, 50), (2112, 853, 107, 50),
+        (1900, 969, 107, 50), (2112, 969, 107, 50),
+        (1900, 1080, 107, 50), (2112, 1080, 107, 50),
+        (1900, 1261, 107, 50), (2112, 1261, 107, 50),
+        (1900, 1446, 107, 50), (2112, 1446, 107, 50),
+
+        (1900, 1782, 107, 50), (2112, 1782, 107, 50),
+        (1900, 2171, 107, 50), (2112, 2171, 107, 50),
+
+        (1900, 2568, 107, 50), (2112, 2568, 107, 50),
+        (1900, 2817, 107, 50), (2112, 2817, 107, 50),
+
+        (1900, 3108, 107, 50), (2112, 3108, 107, 50),
     ]
 
     image = cv2.imread(image_path)
@@ -37,6 +58,11 @@ def is_checked(image_path):
             results.append(True)
         else:
             results.append(False)
+
+    if debug:
+        print(results) # 顯示檢查結果
+        # 繪製並顯示坐標
+        draw_check_coords(image.copy(), check_coords)
 
     return process_results(results)
 
@@ -60,8 +86,10 @@ def process_results(results):
     return processed_results
 
 # 使用範例
-image_path = 'C:\\Users\\junting\\Desktop\\MODA_company_OCR\\temp\\checklist_70565450_page_1.jpg'  # 圖像路徑
+# image_path = 'C:\\Users\\junting\\Desktop\\MODA_company_OCR\\temp\\checklist_16325089_page_1.jpg'
+# image_path = 'C:\\Users\\junting\\Desktop\\MODA_company_OCR\\temp\\checklist_16590299_page_1.jpg'
+image_path = 'C:\\Users\\junting\\Desktop\\MODA_company_OCR\\temp\\checklist_70565450_page_1.jpg'
 
 # 獲取最終結果
-final_results = is_checked(image_path)
+final_results = is_checked(image_path, debug=True)
 print(final_results)
